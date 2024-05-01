0327c67e7457b130c59e0511ffbbf027d4b142e0
aredda <aredda.ibrahim@gmail.com>
2024-05-01 15:58:10 +0100

    feat: adding configuration file

--- lib/wizard.rb
+++ lib/wizard.rb
@@ -4,9 +4,18 @@ require_relative "wizard/version"
 require_relative "wizard/concept_type"
 require_relative "wizard/concept_generator"
 require_relative "wizard/file_helper"
+require_relative "wizard/configuration"
 
 module Wizard
   def self.generate(model)
     "Generating #{model} concept files..."
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure(&block)
+    yield(configuration)
   end
 end
--- test/wizard/test_concept_generator.rb
+++ test/wizard/test_concept_generator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 module Wizard
   class TestConceptGenerator < Minitest::Test
@@ -10,14 +10,24 @@ module Wizard
       assert_equal Wizard::ConceptType::OPERATION, generator.type
     end
 
+    def test_that_it_configures
+      Wizard.configure do |config|
+        config.base_directory = "app/concepts"
+      end
+
+      generator = Wizard::ConceptGenerator.new type: Wizard::ConceptType::FORM
+
+      assert_equal "app/concepts/user/form/create.rb", generator.generate("User", "create")
+    end
+
     def test_that_it_generates_concept
       generator = Wizard::ConceptGenerator.new type: Wizard::ConceptType::OPERATION
 
-      assert_equal 'user/admin/operation/create', generator.generate('User', 'create', 'Admin')
+      assert_equal "app/concepts/user/admin/operation/create.rb", generator.generate("User", "create", "Admin")
 
       generator = Wizard::ConceptGenerator.new type: Wizard::ConceptType::FINDER
 
-      assert_equal 'user/finder/all', generator.generate('User', 'All')
+      assert_equal "app/concepts/user/finder/all.rb", generator.generate("User", "All")
     end
   end
 end
